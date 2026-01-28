ceb0f816817466200206c2aad7dc01816519dbf9
nwquser00001 <antonymaliakkal002@gmail.com>
2026-01-28 22:28:39 +0530

    fix(dashboard & sidebar)

--- src/pages/Dashboard/Components/DataDisplay.jsx
+++ src/pages/Dashboard/Components/DataDisplay.jsx
@@ -5,17 +5,19 @@ import React from 'react'
 function DataDisplay({ count, title, styles = {}, prefix, suffix, precision = 0, icon, loading }) {
   return (
     <Card bordered={false} className='border border-border w-full'>
-      <Flex align='center' gap={100} justify='space-between'>
-        <Statistic
-          title={<p className='text-gray-500 font-bold'>{title}</p>}
-          value={count}
-          precision={precision}
-          valueStyle={{ fontSize: 32, fontWeight: "bolder", ...styles }}
-          prefix={prefix}
-          suffix={suffix}
-          loading={loading}
-        />
-        <div className='bg-primary p-2 rounded-lg flex'>
+      <Flex align='center' gap={16} justify='space-between'>
+        <div className="flex-1 min-w-0">
+          <Statistic
+            title={<p className='text-gray-500 font-bold'>{title}</p>}
+            value={count}
+            precision={precision}
+            valueStyle={{ fontSize: 32, fontWeight: "bolder", whiteSpace: "nowrap", ...styles }}
+            prefix={prefix}
+            suffix={suffix}
+            loading={loading}
+          />
+        </div>
+        <div className='bg-primary p-2 rounded-lg flex flex-shrink-0'>
           {icon}
         </div>
       </Flex>
