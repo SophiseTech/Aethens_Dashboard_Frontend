42cce32e39c7811612cb2bfc84914ce9bcf47df1
jazzirHussain <jasirhussain2002@gmail.com>
2026-02-21 16:21:55 +0530

    changes

--- src/pages/Students/Component/SessionStatusTable.jsx
+++ src/pages/Students/Component/SessionStatusTable.jsx
@@ -6,6 +6,7 @@ import permissions from '@utils/permissions'
 import { Button, Image, Input, Select, Space, Table } from 'antd'
 import React, { useEffect, useState, useMemo } from 'react'
 import { useStore } from 'zustand'
+import userStore from '@stores/UserStore'
 
 const { Search } = Input;
 
@@ -15,7 +16,7 @@ function SessionStatusTable({ student }) {
   const { course } = useStore(courseStore); // Get course from courseStore which has full data
   const [statusFilter, setStatusFilter] = useState('all');
   const [searchText, setSearchText] = useState('');
-
+  const { user } = userStore()
   // Use course from courseStore which has syllabusType and images populated
   const syllabusType = course?.syllabusType || 'general';
 
@@ -122,10 +123,10 @@ function SessionStatusTable({ student }) {
         dataIndex: "action",
         key: "action",
         render: (_, record) => (
-           <>
-          {permissions.session_status.delete.includes(user?.role) &&
-            <Button icon={<RestFilled />} color='red' onClick={() => { deleteFacultyRemark(record._id) }} />
-          }
+          <>
+            {permissions.session_status.delete.includes(user?.role) &&
+              <Button icon={<RestFilled />} color='red' onClick={() => { deleteFacultyRemark(record._id) }} />
+            }
           </>
         ),
       }
